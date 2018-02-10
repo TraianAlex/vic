0106c94f6a8a0ff459e227aaf1b8bfe8a8bcce5c
TraianAlex <victor_traian@yahoo.com>
2018-02-09 20:14:40 -0500

    admin links get the latest

--- app/Http/Controllers/Admin/LinkController.php
+++ app/Http/Controllers/Admin/LinkController.php
@@ -25,7 +25,7 @@ class LinkController extends Controller
     public function index()
     {
         $title = 'Index - link';
-        $links = Link::paginate(15);
+        $links = Link::latest()->paginate(15);
         return view('link.index',compact('links','title'));
     }
 
