52836613ae6d4251aea98d4fa3bc194b3a6bb06d
TraianAlex <victor_traian@yahoo.com>
2019-08-19 16:19:13 -0400

    On branch development 	add delete batch in ip page 	modified:   app/Http/Controllers/Admin/IpController.php 	modified:   dump.rdb 	modified:   resources/views/ip/index.blade.php

--- resources/views/ip/index.blade.php
+++ resources/views/ip/index.blade.php
@@ -49,16 +49,23 @@
 
     <h1>Ip Index</h1>
     <a href='{!!url("ip")!!}/create' class = 'btn btn-success'><i class="fa fa-plus"></i> New</a>
+    <a class="btn btn-success js-delete-page"><i class="fa fa-trash font-size-20"></i> Delete</a>
     <br>
     <br>
     <table class = "table table-striped table-bordered table-hover" style = 'background:#fff'>
         <thead>
+            <th>
+                <input id="checkbox-all" type="checkbox">
+            </th>
             <th>ip</th>
             <th>actions</th>
         </thead>
         <tbody>
             @foreach($ips as $ip)
-            <tr>
+            <tr id="{!!$ip->id!!}>
+                <td>
+                    <input id="{!!$ip->id!!}" name="{!!$ip->page!!}" type="checkbox" data-id="{!!$ip->id!!}">
+                </td>
                 <td>{!!$ip->ip!!}</td>
                 <td>
                     <a data-toggle="modal" data-target="#myModal" class = 'delete btn btn-danger btn-xs' data-link = "/ip/{!!$ip->id!!}/deleteMsg" ><i class = 'fa fa-trash'> delete</i></a>
@@ -73,3 +80,84 @@
 
 </section>
 @endsection
+@section('js')
+    <script>
+        $('.content').find('.js-delete-page').stop().fadeOut(400);
+
+        $('table thead').on('change', 'input[type=checkbox]', function () {
+            const checked = $(this).prop('checked');
+            const $table = $(this).parents('table');
+            if (checked === true) {
+                $table.find('tbody input[type=checkbox]').each(function () {
+                    $(this).prop('checked', true)
+                });
+            } else {
+                $table.find('tbody input[type=checkbox]').each(function () {
+                    $(this).prop('checked', false)
+                });
+            }
+        });
+
+        $('.data-table').on('change', 'input[type=checkbox]', function () {
+            let counter = 0;
+            $(this).parents('.content').find('table tbody input[type=checkbox]:checked').each(function () {
+                counter++;
+            });
+            if (counter > 0) {
+                $(this).parents('.content').find('.js-delete-page').stop().fadeIn(400);
+            } else {
+                $(this).parents('.content').find('.js-delete-page').stop().fadeOut(400);
+            }
+        });
+
+        $('.content').on('click', '.js-delete-page', function (e) {
+            e.preventDefault();
+            const $table = $(this).parents('.content').find('table');
+            const ids = [];
+            $table.find('tbody input[type=checkbox]:checked').each(function () {
+                ids.push($(this).data('id'));
+            });
+            if (ids.length > 0) {
+                if (confirm('Are you sure you want to delete this?')) {
+                    let counter = 0;
+                    const promises = [];
+                    ids.every(function (id) {
+                        let request = $.ajax({
+                            url: '/ip/' + id + '/deleteMsg',
+                            method: 'GET',
+                            dataType: "json",
+                            success: function () {
+                                // const $row = $table.find(id).parents('tr');
+                                // $row.remove();
+                                console.log('Success');
+                            },
+                            error: function () {
+                                counter++;
+                            }
+                        });
+                        $('#'+id).remove();
+                        promises.push(request);
+                        return true;
+                    });
+                    $.when.apply(null, promises)
+                        .done(function () {
+                            $table.parents('.content').find('.js-delete-page').stop().fadeOut(400);
+                            if (counter === 0) {
+                                console.log('Successful!', 'Your pages has been successfully deleted.', 'success');
+                            } else {
+                                console.log('Error!', `${counter} couldn't be deleted, please try again later`, 'error');
+                            }
+                        })
+                        .fail(function () {
+                            console.log('Error!', `Your records couldn't be deleted, please try again later`, 'error');
+                        });
+                }
+            } else {
+                console.log('Error!', 'Please select an item before clicking on the delete button' , 'error');
+            }//if ids where actually selected
+        });
+
+        showMessage("This is an test message in admin page.");
+    </script>
+@endsection
+
