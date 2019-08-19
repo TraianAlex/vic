ed646b64c82a5411543499cc771cb680e1336a19
TraianAlex <victor_traian@yahoo.com>
2019-08-19 17:10:51 -0400

    On branch development 	redirect fix 	modified:   app/Http/Controllers/Admin/StatController.php

--- app/Http/Controllers/Admin/StatController.php
+++ app/Http/Controllers/Admin/StatController.php
@@ -61,7 +61,7 @@ class StatController extends Controller
         $stat = $stat->where('ip', $ip);
         $stat->delete();
         flash('Your data has been deleted!');
-        return redirect()->back();
+        return redirect('stat/ips');//redirect()->back();
     }
 
     protected function extract($collection, $page)
