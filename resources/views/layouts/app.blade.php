560e4ce4f0832fc1e1d77cd954f390def926e9e8
Traian Alexandru <victor_traian@yahoo.com>
2019-08-30 11:32:24 -0400

    On branch development 	new file:   .gitignore 	new file:   app/Http/Controllers/Frontend/PagesController.php 	new file:   config/database.php 	new file:   package-lock.json 	new file:   package.json 	new file:   public/assets/images/screenshot-300x151.png 	new file:   public/css/all.css 	new file:   public/css/app.css 	new file:   public/js/all.js 	new file:   public/js/app.js 	new file:   public/mix-manifest.json 	new file:   public/site_map.xml 	new file:   public/site_map2.xml

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="canonical" href="https://vic.com.ro"/>
+    <link rel="shortcut icon" href="{{asset('assets/images/traian-s-640x426.jpg')}}" type="image/x-icon">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @yield('meta')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" data-turbolinks-track="reload">
+    <link href="{{ mix('css/all.css') }}" rel="stylesheet" data-turbolinks-track="reload">
+    @yield('css')
+    <style type="text/css">
+    .turbolinks-progress-bar {visibility: hidden;}
+    .notify-hide{display: none;}
+      @media (min-width: 991px) {.notifications-menu{display: none;}}
+    </style>
+    <!-- Google Analytics -->
+    <!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-63781170-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+  gtag('config', 'UA-63781170-2');
+</script>
+</head>
+<body>
+    <div id="app">
+        @include('partials.vicnav')
+
+        @yield('content')
+
+        @include('partials.footer')
+    </div>
+    <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;" href="#menu2-1m"><i class="mbri-down mbr-iconfont"></i></a></div>
+    <input name="animation" type="hidden">
+  <script src="{{ asset('js/turbolinks.js') }}" defer></script>
+  <script src="{{ mix('js/all.js') }}" defer data-turbolinks-eval="false" data-turbolinks-track="reload"></script>
+  <script src="{{ mix('js/app.js') }}" data-turbolinks-eval="false" data-turbolinks-track="reload"></script>
+  <script defer data-turbolinks-eval="false" data-turbolinks-track="reload">
+    function showMessage(message) {
+      if (!("Notification" in window)) {
+        // Code to run if notifications are not
+        // supported by the visitor's browser
+      } else {
+        if (Notification.permission === "granted") {
+          var notification = new Notification(message);
+        } else if (Notification.permission !== "denied") {
+          Notification.requestPermission().then(function (permission) {
+            if (permission === "granted") {
+              var notification = new Notification(message);
+            }
+      });
+        }
+      }
+    }
+  </script>
+  @yield('script')
+  <script defer data-turbolinks-eval="false" data-turbolinks-track="reload">
+  (function(){
+      var submitAjaxRequest = function(e){
+          $('.temp').show();
+          var form = $(this);
+          var method = form.find('input[name="_method"]').val() || 'POST';
+          $.ajax({
+              type: method,
+              url: form.prop('action'),
+              data: form.serialize(),
+              success: function(response){
+                  $('.temp').hide();
+                  $('.flash').text(response).fadeIn(500).delay(5000).fadeOut(500);
+                  $('form[data-email]')[0].reset();
+              }
+          });
+          e.preventDefault();
+      }
+      $('form[data-email]').on('submit', submitAjaxRequest);
+  })();
+</script>
+</body>
+</html>
