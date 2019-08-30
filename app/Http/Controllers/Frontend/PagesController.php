8fe209dc7cda2789c7df1966427f28b434412474
Traian Alexandru <victor_traian@yahoo.com>
2019-08-30 11:32:24 -0400

    On branch development 	new file:   .gitignore 	new file:   app/Http/Controllers/Frontend/PagesController.php 	new file:   config/database.php 	new file:   package-lock.json 	new file:   package.json 	new file:   public/assets/images/screenshot-300x151.png 	new file:   public/css/all.css 	new file:   public/css/app.css 	new file:   public/js/all.js 	new file:   public/js/app.js 	new file:   public/mix-manifest.json 	new file:   public/site_map.xml 	new file:   public/site_map2.xml

---
+++ app/Http/Controllers/Frontend/PagesController.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\Http\Controllers\Frontend;
+
+use App\Link;
+use App\Category;
+use App\Jobs\Tracker;
+use App\Mail\ContactForm;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+
+class PagesController extends Controller
+{
+    public function __construct()
+    {
+        dispatch(new Tracker);
+    }
+    public function __invoke()
+    {
+        $page = request()->segment(1) ?? 'index';
+        $captcha = $this->generateChallenge();
+        return view()->exists("pages/$page") ? view("pages/$page", compact('captcha')) : view("pages/index");
+    }
+    public function link()
+    {
+        $links = Link::with('categories')->latest()->paginate(20);
+        $categories = Cache::remember('categories', 60*24*7, function () {
+            return Category::all();
+        });
+        return view('pages.links', compact('links', 'categories'));
+    }
+    public function countLink(Request $request, $id)
+    {
+        $links = Link::findOrFail((int)$request->id);
+        $visits = $links->visits;
+        $links->visits = $visits + 1;
+        $links->save();
+        return redirect($links->address);
+    }
+    public function result(Category $category)
+    {
+        $links = $category->links()->paginate(15);
+        return view('pages.links', compact('links'));
+    }
+    public function all()
+    {
+        $links = Link::with('categories')->latest()->paginate(2000);
+        return view('pages.links', compact('links'));
+    }
+    public function sendEmail(Request $request)
+    {
+        $captcha = $request->get('s_q');
+        if ($this->validate_email($captcha)) {
+            Mail::to('victor_traian@yahoo.com')
+                ->queue(new ContactForm($request->name, $request->email, $request->message));
+            echo "Thanks for filling out form!";//flash('Thanks for filling out the form!');//return back();
+        } else {
+            echo "You must provide a correct answer!";
+        }
+    }
+    public function loadGrid()
+    {
+        $url = Storage::url('image.x');
+        if (file_exists(getcwd().$url)) {
+            return file_get_contents(getcwd().$url);
+        }
+    }
+    private function validate_email($resp)
+    {
+        $val = (int)session('challenge');
+        session(['challenge' => null]);
+        if ($resp != $val) {
+            return false;
+        }
+        return true;
+    }
+    private function generateChallenge()
+    {
+        $numbers = [mt_rand(1, 4), mt_rand(1, 4)];
+        session(['challenge' => $numbers[0] + $numbers[1]]);
+        $converted = array_map('ord', $numbers);
+        return "<input type=\"text\" name=\"s_q\" class=\"form-control input\"
+      placeholder='&#87;&#104;&#97;&#116;&#32;&#105;&#115;&#32;
+      &#$converted[0];&#32;&#43;&#32;&#$converted[1];&#63;'>";
+    }
+    // public function saveDraw(Request $request)
+    // {
+    //     $url = Storage::url('image.x');
+    //     file_put_contents($url, json_encode($_POST));
+    //     return back();
+    //     //file_exist(storage_path('app/public'));
+    //     //\File::put(storage_path('app/public/storage/image.x'), json_encode($_POST));
+    //     //\File::delete(storage_path('app/public/storage/image.x'));
+    // }
+}
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+@section('meta')
+<meta name="description" content="Web Development Private Service. Responsive mobile websites. Webdesign and implementation.">
+<meta name="keywords" content="custom website, web services, webdesign development, mobile web design, personal website, traian alexandru">
+<title>Website Development Services</title>
+@endsection
+@section('content')
+@include('pages.headers.services')
+<section class="cid-qwsz03Q8gE" id="pricing-tables2-13">
+    <div class="container">
+        <div class="media-container-row">
+            <div class="plan col-12 mx-2 my-2 justify-content-center col-lg-4">
+                <div class="plan-header text-center pt-5">
+                    <h3 class="plan-title mbr-fonts-style display-5">
+                        Lite
+                    </h3>
+                    <div class="plan-price">
+                        <span class="price-value mbr-fonts-style display-5"></span>
+                        <span class="price-figure mbr-fonts-style display-1"></span>
+                        <small class="price-term mbr-fonts-style display-7"></small>
+                    </div>
+                </div>
+                <div class="plan-body">
+                    <div class="plan-list align-center">
+                        <ul class="list-group list-group-flush mbr-fonts-style display-7">
+                            <li class="list-group-item">Package contains static website, which includes non-manageable pages, mobile webdesign, sitemap, progressive web app.</li><br><li class="list-group-item">~ $500</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="plan col-12 mx-2 my-2 justify-content-center favorite col-lg-4">
+                <div class="plan-header text-center pt-5">
+                    <h3 class="plan-title mbr-fonts-style display-5">
+                        Standard
+                    </h3>
+                    <div class="plan-price">
+                        <span class="price-value mbr-fonts-style display-5"></span>
+                        <span class="price-figure mbr-fonts-style display-1"></span>
+                        <small class="price-term mbr-fonts-style display-7"></small>
+                    </div>
+                </div>
+                <div class="plan-body">
+                    <div class="plan-list align-center">
+                        <ul class="list-group list-group-flush mbr-fonts-style display-7">
+                            <li class="list-group-item">Package includes pages where you can manage a part of them, mobile webdesign, database, sitemap, progressive webb app.</li><br><li class="list-group-item">~ $900</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="plan col-12 mx-2 my-2 justify-content-center col-lg-4">
+                <div class="plan-header text-center pt-5">
+                    <h3 class="plan-title mbr-fonts-style display-5">
+                        Advanced
+                    </h3>
+                    <div class="plan-price">
+                        <span class="price-value mbr-fonts-style display-5"></span>
+                        <span class="price-figure mbr-fonts-style display-1"></span>
+                        <small class="price-term mbr-fonts-style display-7"></small>
+                    </div>
+                </div>
+                <div class="plan-body">
+                    <div class="plan-list align-center">
+                        <ul class="list-group list-group-flush mbr-fonts-style display-7">
+                            <li class="list-group-item">Package includes manageable pages and eCommerce features, mobile webdesign, database, sitemap, progressive web app.</li><br><li class="list-group-item"><a href="contact.html">Contact Us</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="mbr-section content8 cid-qwPzbpLJTt" id="content8-19" data-rv-view="74">
+    <div class="container">
+        <div class="media-container-row title">
+            <div class="col-12 col-md-8">
+                <div class="mbr-section-btn align-center"><a class="btn btn-warning display-4" href="contact">Request a quote</a></div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+@section('script')
+<script>
+    showMessage("Browser notification: This is a test message in services page");
+</script>
+@endsection
