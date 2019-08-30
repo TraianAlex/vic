a2ca8dd7f6404e53b69ad9343bcc5497c0bfb2f0
Traian Alexandru <victor_traian@yahoo.com>
2019-08-30 13:25:14 -0400

    On branch development 	new file:   resources/views/pages/about.blade.php 	new file:   resources/views/pages/headers/about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app')
+@section('meta')
+    <meta name="description" content="Traian Alexandru Web Developer Toronto. Build responsive mobile websites. Webdesign and implementation.">
+    <meta name="keywords" content="website design, web page design, webdesign, web development, mobile, traian alexandru">
+    <title>Mobile Web Development</title>
+@endsection
+@section('content')
+    @include('pages.headers.about')
+    <section class="mbr-section article content9 cid-qCbNPo4W1h" id="content9-2b" data-rv-view="784">
+        <div class="container">
+            <div class="inner-container" style="width: 100%;">
+                <div class="section-text align-center mbr-fonts-style display-5"><a href="https://github.com/TraianAlex/minimvc" class="text-black"><strong>-&nbsp;Mini MVC framework&nbsp;basic</strong></a><br><br><a href="https://github.com/TraianAlex/restful-api-55" target="_blank" class="text-black"><strong>- Restful-APi - Laravel55</strong></a><br><br><a href="https://github.com/TraianAlex/dev" class="text-black" target="_blank"><strong>-&nbsp;Quick shopping list Angular - Laravel</strong></a><br><br><a href="https://github.com/TraianAlex/medium-login" class="text-black" target="_blank"><strong>- Login without password using your email - Laravel</strong></a><br><br><a href="https://github.com/TraianAlex/restfull-api2" class="text-black" target="_blank"><strong>- Angular on Laravel Restful Api</strong></a><br><br><a href="https://github.com/TraianAlex/myshop" class="text-black" target="_blank"><strong>-&nbsp;Laravel and Stripe</strong></a><br><br><a href="https://github.com/TraianAlex/git-user-admin-auth" class="text-black" target="_blank"><strong>-&nbsp;Separate admin authentication in Laravel</strong></a></div>
+            </div>
+        </div>
+    </section>
+    <section class="progress-bars3 cid-qwshY2hf5v" id="progress-bars3-11" data-rv-view="117">
+        <div class="container">
+            <div class="media-container-row pt-5 mt-2">
+                <div class="card p-3 align-center">
+                    <div class="wrap">
+                        <div class="pie_progress progress1" role="progressbar" data-goal="100">
+                            <p class="pie_progress__number mbr-fonts-style display-5">100%</p>
+                        </div>
+                    </div>
+                    <div class="mbr-crt-title pt-3">
+                        <h4 class="card-title py-2 mbr-fonts-style display-5">HTML</h4>
+                    </div>
+                </div>
+                <div class="card p-3 align-center">
+                    <div class="wrap">
+                        <div class="pie_progress progress2" role="progressbar" data-goal="80">
+                            <p class="pie_progress__number mbr-fonts-style display-5">80%</p>
+                        </div>
+                    </div>
+                    <div class="mbr-crt-title pt-3">
+                        <h4 class="card-title py-2 mbr-fonts-style display-5">CSS</h4>
+                    </div>
+                </div>
+                <div class="card p-3 align-center">
+                    <div class="wrap">
+                        <div class="pie_progress progress3" role="progressbar" data-goal="70">
+                            <p class="pie_progress__number mbr-fonts-style display-5">70%</p>
+                        </div>
+                    </div>
+                    <div class="mbr-crt-title pt-3">
+                        <h4 class="card-title py-2 mbr-fonts-style display-5">jQuery / React</h4>
+                    </div>
+                </div>
+                <div class="card p-3 align-center">
+                    <div class="wrap">
+                        <div class="pie_progress progress4" role="progressbar" data-goal="90">
+                            <p class="pie_progress__number mbr-fonts-style display-5">90%</p>
+                        </div>
+                    </div>
+                    <div class="mbr-crt-title pt-3">
+                        <h4 class="card-title py-2 mbr-fonts-style display-5">PHP</h4>
+                    </div>
+                </div>
+                <div class="card p-3 align-center">
+                    <div class="wrap">
+                        <div class="pie_progress progress5" role="progressbar" data-goal="80">
+                            <p class="pie_progress__number mbr-fonts-style display-5">80%</p>
+                        </div>
+                    </div>
+                    <div class="mbr-crt-title pt-3">
+                        <h4 class="card-title py-2 mbr-fonts-style display-5">Laravel / Symfony</h4>
+                    </div>
+                </div>
+                <div class="card p-3 align-center">
+                    <div class="wrap">
+                        <div class="pie_progress progress6" role="progressbar" data-goal="90">
+                            <p class="pie_progress__number mbr-fonts-style display-5">90%</p>
+                        </div>
+                    </div>
+                    <div class="mbr-crt-title pt-3">
+                        <h4 class="card-title py-2 mbr-fonts-style display-5">Bootstrap</h4>
+                    </div>
+                </div>
+            </div>
+        </div></section>
+@endsection
+@section('script')
+    <script src="{{asset('assets/as-pie-progress/jquery-as-pie-progress.min.js')}}" defer data-turbolinks-track="reload"></script>
+@endsection
