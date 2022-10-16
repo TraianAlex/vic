fbc3785169cef9e0967228e9740c5aaf2aefd23d
Traian Alexandru <victor_traian@yahoo.com>
2022-10-16 17:04:08 -0400

    test

--- app/Http/Controllers/Admin/IpController.php
+++ app/Http/Controllers/Admin/IpController.php
@@ -35,7 +35,7 @@ class IpController extends Controller
             //->orderBy('page')->get();//->reverse()->unique('page');
         } else $pagesByIP = null;
 
-        $ips = Ip::paginate(500);
+        $ips = Ip::paginate(400);
         return view('ip.index',compact('ips','title', 'ipss', 'pagesByIP'));
     }
 
