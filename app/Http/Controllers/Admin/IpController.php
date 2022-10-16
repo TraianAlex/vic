dd76508e049f502799376802b81ca799c6fb7088
Traian Alexandru <victor_traian@yahoo.com>
2022-10-16 16:53:23 -0400

    test

--- app/Http/Controllers/Admin/IpController.php
+++ app/Http/Controllers/Admin/IpController.php
@@ -26,7 +26,7 @@ class IpController extends Controller
     public function index(Request $request)
     {
         $title = 'Index - ip';
-        $ipss = Ip::with('pages')->paginate(50);//->sortByDesc('created_at');
+        $ipss = Ip::with('pages')->paginate(250);//->sortByDesc('created_at');
 
         if(isset($request->id)){
             $pagesByIP = Ip::with(['pages' => function($query){
