2185fd0199f4017314358bf23d39afbeaa48772d
Traian Alexandru <victor_traian@yahoo.com>
2022-10-16 16:11:50 -0400

    dec admin page pagination

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -25,7 +25,7 @@ class PageController extends Controller
     public function index()
     {
         $title = 'Index - page';
-        $pages = Page::orderBy('page')->paginate(300);
+        $pages = Page::orderBy('page')->paginate(250);
         return view('page.index', compact('pages', 'title'));
     }
 
